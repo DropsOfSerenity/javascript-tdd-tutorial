5d6c1585f8db1aec02bd0da9804ec02867e5063f
Justin Schiff <AmbientRevolution@gmail.com>
2015-02-15 22:40:14 -0800

    wrap in self executing function

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,12 +1,14 @@
 /* global describe, it, require */
-'use strict';
+(function() {
+  "use strict";
 
-var assert = require('chai').assert,
-    expect = require('chai').expect,
-    server = require('./server');
+  var assert = require("chai").assert,
+  expect = require("chai").expect,
+    server = require("./server");
 
-describe('server', function() {
-  it('should expose a number function', function() {
-    expect(server.number()).to.equal(3);
+  describe("server", function() {
+    it("should expose a number function", function() {
+      expect(server.number()).to.equal(3);
+    });
   });
-});
+})();
