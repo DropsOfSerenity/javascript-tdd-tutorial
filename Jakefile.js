68b63b72e6404a47fc26c375532945b1fbae1e2a
Justin Schiff <AmbientRevolution@gmail.com>
2015-02-15 21:18:56 -0800

    linting task good to go

--- Jakefile.js
+++ Jakefile.js
@@ -1,3 +1,7 @@
+/* global desc, task, jake, file, complete */
+
+"use strict";
+
 desc("Build and test");
 task("default", ["lint"], function() {
   console.log("default");
@@ -7,5 +11,14 @@ desc("Lint the code");
 task("lint", [], function() {
   console.log("Lint code goes here");
   var lint = require("./build/lint/lint.js");
-  lint.validateFile("Jakefile.js", {}, {});
+  var files = new jake.FileList();
+  files.include("**/*.js");
+  files.exclude("node_modules");
+  files.exclude("build");
+
+  var options = {
+    node: true
+  };
+
+  lint.validateFileList(files.toArray(), options, {});
 });
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Following along with node/javascript test driven development tutorial
+
+Assumes presence of node.js
+Run with `./jake.sh`
